e63bbe7335f7aecacca74bdaa1e012e6f133a0b4
stack247 <suhendra.hui@outlook.com>
2014-12-14 00:42:36 -0600

    Add file to be tracked

---
+++ Semplice.Kiriwa.DAL/Entity/KiriwaContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Semplice.Kiriwa.Domains;
+
+namespace Semplice.Kiriwa.DAL.Entity
+{
+    public class KiriwaContext : DbContext
+    {
+        public KiriwaContext()
+            : base("Kiriwa.SqlServer")
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+    }
+}

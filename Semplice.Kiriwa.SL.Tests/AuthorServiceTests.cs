732ab567994b751d1798b1723cbc8fe879469e58
stack247 <suhendra.hui@outlook.com>
2014-12-16 11:18:30 -0600

    fix mocking data context in test

--- Semplice.Kiriwa.SL.Tests/AuthorServiceTests.cs
+++ Semplice.Kiriwa.SL.Tests/AuthorServiceTests.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Highway.Data;
 using Highway.Data.Contexts;
+using Moq;
 using NUnit.Framework;
 using Semplice.Kiriwa.Domains;
 
@@ -14,18 +16,30 @@ namespace Semplice.Kiriwa.SL.Tests
     public class AuthorServiceTests
     {
         [TestCase]
-        public void m_s_b()
+        public void m_s_2b()
         {
             // Arrange
-            var _context = new InMemoryDataContext();
-            _context.Add(new Author
-            {
-                AuthorId = 1,
-                FirstName = "Brant",
-                LastName = "Meail"
-            });
+            // InMemoryDataContext() doesn't seem to work. AuthorService can't find the Author in the repository.
+            //var _context = new InMemoryDataContext();
+            var _context = new Mock<IDataContext>();
+            _context.Setup(x => x.AsQueryable<Author>())
+                .Returns(new List<Author>
+                {
+                    new Author
+                    {
+                        AuthorId = 1,
+                        FirstName = "Brant",
+                        LastName = "Meail"
+                    },
+                    new Author
+                    {
+                        AuthorId = 2,
+                        FirstName = "Lapat",
+                        LastName = "Aheq"
+                    }
+                }.AsQueryable());
 
-            var _service = new AuthorService(new Repository(_context));
+            var _service = new AuthorService(new Repository(_context.Object));
 
             // Act
             var _result = _service.GetAuthor(1);

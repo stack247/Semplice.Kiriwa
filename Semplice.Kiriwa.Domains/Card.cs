2805a5d94b4aabe15abe6a5cb8ca4a2e2394e446
stack247 <suhendra.hui@outlook.com>
2014-12-19 19:24:58 -0600

    Added new domains

---
+++ Semplice.Kiriwa.Domains/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semplice.Kiriwa.Domains
+{
+    public class Card
+    {
+        public int CardId { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ Semplice.Kiriwa.Domains/Stack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semplice.Kiriwa.Domains
+{
+    public class Stack
+    {
+        public int StackId { get; set; }
+        public string Name { get; set; }
+
+        public HashSet<Card> Cards { get; set; }
+    }
+}
